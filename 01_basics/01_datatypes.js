f80b10ce4428bcba3d0c489f032d5148ccc76431
akshit-wadhwa <112195833+akshit-wadhwa@users.noreply.github.com>
2024-12-21 17:16:22 +0000

    datatypes and variables

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3) // this will not work because we are using node.js not browser
+
+// typeof - gives datatype of the variable
+
+console.log(typeof "abc")
+
+// number - 2 to power 53
+// bigint
+// string - ""
+// boolean - true, false
+// null - standalone value
+// undefined
+// symbol - unique
+
+// object
+
+console.log(typeof null); // ans - object
+console.log(typeof undefined);// ans - undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const name = "Akshit"
+let age = 21
+var salary = 0
+Status = "Student"
+let city;
+
+
+/*
+Prefer not to use var
+because of block scope and functional scope
+*/
+console.table([name,age, salary, Status, city]);
