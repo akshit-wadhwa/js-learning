7d56a30e59f4b3acbe25bc89e0ac3fab699d39ea
akshit-wadhwa <112195833+akshit-wadhwa@users.noreply.github.com>
2024-12-30 09:53:55 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "Akshit"
+const repoCount = 7
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // Prefered way of fetching string values.
+const mail = new String("Akshit-gmail-com")
+
+console.log(mail[0])
+console.log(mail.slice(0,6))
+console.log(mail.split("-"))
+console.log(mail.length)
+console.log(mail.__proto__);
